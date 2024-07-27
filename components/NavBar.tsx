7b44a6a8946eed541085f9cfdca4690b153526a4
khang-dao <khangcs@outlook.com>
2024-07-27 18:38:21 +1000

    feat: create basic navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,16 +1,19 @@
-import Link from 'next/link';
-import React from 'react';
+import { BiSolidHome as HomeIcon} from "react-icons/bi";
+import { IoMdMoon as MoonIcon} from "react-icons/io";
+import Link from "next/link";
 
 const NavBar: React.FC = () => {
-    return (
-        <nav>
-            <Link className="text-3xl font-bold bg-teal-500" href="/">Home</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/about">About</Link>
-            <Link href="/toolkit">Toolkit</Link>
-            <Link href="/contact">Contact</Link>
-        </nav>
-    );
+  return (
+    <nav className='w-9/12 flex border justify-between items-center border-emerald-500'>
+      <HomeIcon className="border-emerald-700 border" />
+      <Link className="border-emerald-700 border" href="/">Home</Link>
+      <Link className="border-emerald-700 border" href="/projects">Projects</Link>
+      <Link className="border-emerald-700 border" href="/about">About</Link>
+      <Link className="border-emerald-700 border" href="/toolkit">Toolkit</Link>
+      <Link className="border-emerald-700 border" href="/contact">Contact</Link>
+      <MoonIcon className="border-emerald-700 border" />
+    </nav>
+  );
 };
 
 export default NavBar;
