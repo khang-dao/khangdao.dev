636d0ac24484409c6e8f539f57345da77c016307
khang-dao <86513733+khang-dao@users.noreply.github.com>
2024-07-27 23:48:21 +1000

    feat: create about page (#6)
    * config: add path alias for components
    
    * feat: create about page

---
+++ components/PageTitle.tsx
@@ -0,0 +1,11 @@
+
+const PageTitle: React.FC<{ children: string }> = ({ children }) => {
+  return (
+    <div className="relative inline-block m-20">
+      <div className="absolute inset-0 top-4 left-2 bg-[#E8F3FF] z-0 text-box rounded-md blur-[2px]"></div>
+      <span className='relative z-10 text-4xl font-lekton font-bold'>{children}</span>
+    </div>
+  );
+};
+
+export default PageTitle;
