18e777cde766a123ae11bedd1e31fc3fd8b06aaa
khang-dao <86513733+khang-dao@users.noreply.github.com>
2024-08-03 16:14:39 +1000

    setup: deploy github pages via workflow (#10)
    * setup: github actions workflow & config
    
    * fix: linting errors
    
    * chore: uncomment image elem
    
    * fix: linting errors
    
    * fix: tailwind not applying css to prod build

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-function Button({ children }) {
+function Button({ children }: { children: React.ReactNode }) {
   return (
     <button
       className="cursor-default rounded-md bg-[#4C9EFF] px-5 py-2.5 font-lekton text-white"
--- components/form/FormItem.tsx
+++ components/form/FormItem.tsx
@@ -1,12 +1,12 @@
-function FormItem({ children, id, name, type, className }) {
+function FormItem({ children, id, name, type, className }: { children: React.ReactNode, id: string, name: string, type: string, className: string }) {
   return (
     <div>
       {type === "textarea" ? (
         <textarea
           id={id}
           name={name}
-          placeholder={children}
-          className={`${className} resize-none rounded-lg border border-[3px] border-[#DAEBFF] p-2 font-lekton text-[#ACACAC] w-full`}
+          placeholder={children as string}
+          className={`${className} resize-none rounded-lg border-[3px] border-[#DAEBFF] p-2 font-lekton text-[#ACACAC] w-full`}
           required
         />
       ) : (
@@ -14,8 +14,8 @@ function FormItem({ children, id, name, type, className }) {
           type={type}
           id={id}
           name={name}
-          placeholder={children}
-          className={`${className} rounded-lg border border-[3px] border-[#DAEBFF] p-2 font-lekton text-[#ACACAC] w-full`}
+          placeholder={children as string}
+          className={`${className} rounded-lg border-[3px] border-[#DAEBFF] p-2 font-lekton text-[#ACACAC] w-full`}
           required
         />
       )}
