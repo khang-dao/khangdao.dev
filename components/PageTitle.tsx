d9a4de0acc88e0ce6544285c75b21142a82a8125
khang-dao <khangcs@outlook.com>
2024-08-06 16:00:51 +1000

    fix: header taking up full width of flex

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -5,9 +5,9 @@ interface PageTitleProps {
 
 export default function PageTitle({ children, className }: PageTitleProps) {
   return (
-    <div className={`relative mb-10 inline-block ${className}`}>
-      <div className="absolute inset-0 left-2 top-4 z-0 -mr-2 rounded-md bg-[#cfe6ff] blur-[2px]"></div>
-      <span className="relative z-10 font-spartan text-4xl font-bold">{children}</span>
+    <div className="relative mb-5 inline-block">
+      <div className="absolute inset-0 left-2 top-4 z-0 -mr-3 rounded-md bg-[#cfe6ff] blur-[2px]"></div>
+      <span className={`relative z-10 font-spartan text-4xl font-bold ${className}`}>{children}</span>
     </div>
   );
 }
