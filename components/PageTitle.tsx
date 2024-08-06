f52ad037b2ab2789d0871460f036d73decbd8236
khang-dao <khangcs@outlook.com>
2024-08-06 16:14:00 +1000

    feat: update component text colour

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -7,7 +7,7 @@ export default function PageTitle({ children, className }: PageTitleProps) {
   return (
     <div className="relative mb-5 inline-block">
       <div className="absolute inset-0 left-2 top-4 z-0 -mr-3 rounded-md bg-[#cfe6ff] blur-[2px]"></div>
-      <span className={`relative z-10 font-spartan text-4xl font-bold ${className}`}>{children}</span>
+      <span className={`relative z-10 font-spartan text-4xl font-bold text-[#185196] ${className}`}>{children}</span>
     </div>
   );
 }
