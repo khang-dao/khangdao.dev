1917da8c83f4567ee6bfe2c159f4afcd0cbf3de9
khang-dao <khangcs@outlook.com>
2024-08-10 16:52:21 +1000

    chore: remove unnecessary z-positioning

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -2,10 +2,18 @@ import Link from "next/link";
 
 export default function NavMenu({ toggleMenu, isMenuVisible }) {
   return (
-    <nav className={`bg-blue-secondary flex h-screen w-screen flex-col items-center justify-center gap-10 font-spartan text-4xl font-bold text-white ${isMenuVisible ? 'animate-slideDown' : 'hidden'}`}>
-      <Link href="/#projects" onClick={toggleMenu}>Projects</Link>
-      <Link href="/#about" onClick={toggleMenu}>About Me</Link>
-      <Link href="/#contact" onClick={toggleMenu}>Contact</Link>
+    <nav
+      className={`bg-blue-secondary flex h-screen w-screen flex-col items-center justify-center gap-10 font-spartan text-4xl font-bold text-white ${isMenuVisible ? "animate-slideDown" : "hidden"}`}
+    >
+      <Link href="/#projects" onClick={toggleMenu}>
+        Projects
+      </Link>
+      <Link href="/#about" onClick={toggleMenu}>
+        About Me
+      </Link>
+      <Link href="/#contact" onClick={toggleMenu}>
+        Contact
+      </Link>
     </nav>
   );
 }
--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -6,8 +6,8 @@ interface PageTitleProps {
 export default function PageTitle({ children, className }: PageTitleProps) {
   return (
     <div className="relative mb-5 inline-block">
-      <div className="absolute inset-0 left-2 top-4 z-0 -mr-3 rounded-md bg-blue-dark-accent blur-[2px]"></div>
-      <span className={`relative z-10 font-spartan text-4xl font-bold text-blue-dark ${className}`}>{children}</span>
+      <div className="absolute inset-0 left-2 top-4 -mr-3 rounded-md bg-blue-dark-accent blur-[2px]"></div>
+      <span className={`relative font-spartan text-4xl font-bold text-blue-dark ${className}`}>{children}</span>
     </div>
   );
 }
