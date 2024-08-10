86e65f78d7ed0e3bade2ba9e027782dac18a02c4
khang-dao <khangcs@outlook.com>
2024-08-10 15:48:01 +1000

    chore: implement state logic for conditional menu rendering

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,21 @@
-import { GiHamburgerMenu as HamburgerMenuIcon } from "react-icons/gi";
+'use client';
+
 import NavMenu from "@components/NavMenu";
+import { useState } from "react";
+import { GiHamburgerMenu as HamburgerMenuIcon } from "react-icons/gi";
 
 export default function Header() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuVisible(prev => !prev)
+    console.log(isMenuVisible)
+  }
+
   return (
     <header className="flex flex-1 items-start justify-between">
-      <NavMenu />
-      <HamburgerMenuIcon className="fixed ml-10 mt-10 cursor-pointer" color="#469AFF" size={25} />
+      {isMenuVisible && <NavMenu />}
+      <HamburgerMenuIcon className="fixed ml-10 mt-10 cursor-pointer" color="#469AFF" size={25} onClick={toggleMenu} />
     </header>
   );
 }
--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavMenu() {
   return (
-    <nav className="flex h-screen w-[65vw] flex-col items-center justify-center border-purple-500 border-8 gap-10 bg-blue-dark-accent">
+    <nav className="bg-blue-secondary font-spartan text-4xl font-bold flex h-screen w-[65vw] flex-col items-center justify-center gap-10 border-8 border-purple-500 text-white">
       <Link href="/#projects">Projects</Link>
       <Link href="/#about">About Me</Link>
       <Link href="/#contact">Contact</Link>
