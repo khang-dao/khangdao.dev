04dd81ddc1ee53d43b100a98163d2f992539673b
khang-dao <khangcs@outlook.com>
2024-08-11 14:41:03 +1000

    feat: update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
-import "./styles/globals.css";
+/* eslint-disable react-refresh/only-export-components */
 import Head from "next/head";
+import "./styles/globals.css";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Khang Dao',
+  description: 'The official portfolio website of Khang Dao.',
+};
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -9,7 +16,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html className="scroll-smooth" lang="en">
       <Head>
-        <title>Khang Dao</title>
+        <link rel="icon" href="./favicon.ico" sizes="any"/>
       </Head>
       <body className="before:pointer-events-none before:fixed before:left-0 before:top-0 before:box-border before:h-full before:w-full before:border-[1.25rem] before:border-white before:content-[''] after:pointer-events-none after:fixed after:left-0 after:top-0 after:box-border after:h-full after:w-full after:border-[1.25rem] after:border-white after:content-['']">
         {children}
