76c6ef5d91c58f29490b5d752530cb17f2db080b
khang-dao <86513733+khang-dao@users.noreply.github.com>
2024-08-11 18:42:06 +1000

    feat: implement mobile friendly layout (#15)
    * feat: update favicon
    
    * temp: comment out placeholder div
    
    * chore: update viewport scaling
    
    * chore: remove unnecessary div
    
    * feat: add basic responsiveness
    
    * feat: make contact section responsive
    
    * feat: make projects section responsive
    
    * feat: make intro section responsive
    
    * fix: down arrow not showing on phone

--- app/(sections)/IntroSection.tsx
+++ app/(sections)/IntroSection.tsx
@@ -4,21 +4,21 @@ import Image from "next/image";
 
 export default function IntroSection() {
   return (
-    <section className="flex h-screen w-[65vw] flex-col items-center justify-between">
-      <div className="flex w-full grow items-center justify-between">
-        <div className="flex w-3/5 flex-col">
+    <section className="flex h-screen w-[65vw] flex-col items-center lg:justify-between">
+      <div className="flex w-full grow flex-col items-center justify-center gap-28 lg:flex-row lg:justify-between">
+        <div className="flex flex-col items-center lg:items-start lg:w-3/5">
           <div className="relative">
             <div className="max-w-3xl">
-              <PageTitle className="text-5xl">Hi, I&#39;m Khang</PageTitle>
+              <PageTitle className="text-4xl lg:text-5xl">Hi, I&#39;m Khang</PageTitle>
             </div>
           </div>
 
-          <p className="font-spartan text-2xl font-light">
+          <p className="text-center lg:text-justify font-spartan text-2xl font-light">
             A versatile <span className="text-blue-secondary">developer</span> with a passion for creation, automation,
             and cloud technologies ☁️
           </p>
         </div>
-        <div className="relative h-64 w-64">
+        <div className="relative h-48 w-48 md:h-80 md:w-80 lg:h-64 lg:w-64">
           <Image
             src="images/blue_dragon.jpg"
             alt="Blue Dragon"
@@ -30,7 +30,7 @@ export default function IntroSection() {
         </div>
       </div>
 
-      <DownArrowIcon className="mb-14 animate-bob" color="#469AFF" size={25} />
+      <DownArrowIcon className="mb-28 lg:mb-14 animate-bob" color="#469AFF" size={25} />
     </section>
   );
 }
