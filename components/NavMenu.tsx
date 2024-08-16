e76898f83d49eaa91c14bc9ac189b6ba97082d47
khang-dao <khangcs@outlook.com>
2024-08-16 22:23:19 +1000

    fix: nav menu dropdown animation

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -2,14 +2,11 @@ import useScrollDisable from "hooks/useScrollDisable";
 import Link from "next/link";
 
 export default function NavMenu({ toggleMenu, isMenuVisible }) {
-
-  useScrollDisable(isMenuVisible)
+  useScrollDisable(isMenuVisible);
 
   return (
     <nav
-      className={`bg-blue-secondary flex h-screen w-screen flex-col items-center justify-center gap-10 font-spartan text-4xl font-bold text-white 
-        ${isMenuVisible ? 'animate-slide-down' : 'opacity-0 hidden animate-slide-up'} 
-        transition-opacity duration-300 ease-in-out`}
+      className={`flex h-screen w-screen flex-col items-center justify-center gap-10 bg-blue-secondary font-spartan text-4xl font-bold text-white ${isMenuVisible ? "animate-slide-down" : "animate-slide-up opacity-0"} transition-opacity duration-300 ease-in-out`}
     >
       <Link href="/#projects" onClick={toggleMenu}>
         Projects
